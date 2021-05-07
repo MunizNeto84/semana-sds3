337b994ec46ad689e0b784b2515d5a2dca9ad8c0
munizneto84 <muniznetto@outlook.com>
2021-05-07 01:09:59 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer/Index";
-import NavBar from "components/NavBar";
-import React from "react";
+import BarChart from "components/BarChart"
+import DataTable from "components/DataTable"
+import DonutChart from "components/DonutChart"
+import Footer from "components/Footer/Index"
+import NavBar from "components/NavBar"
+import React from "react"
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
       <h1 className='text-primary py-3'>Dashboard de Vendas</h1>
       <div className="row px-3">
         <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Taxa de Sucesso (%) </h5>
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
           <BarChart/>
         </div>
         <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Todas Vendas</h5>
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
           <DonutChart/>
         </div>
       </div>
